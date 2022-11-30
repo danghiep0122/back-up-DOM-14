277a5d210c6df554706c916daa6286e56f6e7d0c
Nguyen Dang Hiep <99936776+danghiep0122@users.noreply.github.com>
2022-11-30 13:40:11 +0700

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import Menu from './pages/Menu'
+import Account from './pages/Account'
+import Promotion from './pages/Promotion'
+import './App.scss'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="promotion" element={<Promotion />} />
+      <Route path="cart" element={<Cart />} />
+      <Route path="menu" element={<Menu />} />
+      <Route path="account-info" element={<Account />} />
+    </Routes>
+  )
+}
+
+export default App

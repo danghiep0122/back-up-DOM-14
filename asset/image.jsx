5757e05d5482b08e8cfdd3b044d16716441f2580
Nguyen Dang Hiep <99936776+danghiep0122@users.noreply.github.com>
2022-11-30 13:40:36 +0700

    Create image.jsx

---
+++ asset/image.jsx
@@ -0,0 +1,13 @@
+const images = {
+  flagVn: require('./flag-vn.png'),
+  flagEn: require('./flag-en.png'),
+  credential: require('./credentials.png'),
+  modalRegister: require('./modal-signin-signup.png'),
+  pizzaBase: require('./productModal/pizza-base.png'),
+  pizzaSize1: require('./productModal/pizza-size-1.png'),
+  pizzaSize2: require('./productModal/pizza-size-2.png'),
+  pizzaSize3: require('./productModal/pizza-size-3.png'),
+  blackZone: require('./black-zone.png'),
+}
+
+export default images
